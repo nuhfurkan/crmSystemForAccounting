c17c3027fe24fb63a633b7c00b1b2a77823ce6a9
Furkan Erturk <furkanerturk@Furkan-MacBook-Air.local>
2021-11-20 17:31:08 +0200

    Some unnecessary changes

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/client")
+def client():
+    return render_template("client.html")
+
+@app.route("/employee")
+def client():
+    return render_template("employee.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug = True)
