903e37f1f395d6440f9e3264371e4b39867ec9df
Furkan Erturk <furkanerturk@Furkan-MacBook-Air.local>
2021-11-22 09:29:27 +0200

    Table creation order changed

--- sql_queries.sql
+++ sql_queries.sql
@@ -7,12 +7,61 @@ CREATE TABLE ToVerify (
     verification_code varchar(20)
 );
 
+CREATE TABLE Currency (
+    id int NOT NULL PRIMARY KEY,
+    name varchar(20),
+    code varchar(5)
+);
+
 CREATE TABLE Users (
     id int PRIMARY KEY Not NUll,
 	first_name varchar(60),
     last_name varchar(60),
     user_name varchar(60),
     user_pass varchar(60)
+);
+
+CREATE TABLE Company (
+    id int NOT NULL PRIMARY KEY,
+	name varchar(30) NOT NULL,
+	default_currency int NOT NULL,
+    FOREIGN KEY (default_currency) REFERENCES Currency(id)
+);
+
+CREATE TABLE CompanyDetails (
+    id int NOT NULL PRIMARY KEY,
+	company int NOT NULL,
+	address varchar(30),
+    zip varchar(30),
+    city varchar(30),
+    country varchar(30),
+    email varchar(30),
+    phone varchar(30),
+    vat_id varchar(30),
+    commerce_id varchar(30),
+    FOREIGN KEY (company) REFERENCES Company(id)
+);
+
+CREATE TABLE Client (
+    id int NOT NULL PRIMARY KEY,
+	company int NOT NULL,
+	name varchar(40) NOT NULL,
+    uuid varchar(40) NOT NULL,
+    FOREIGN KEY (company) REFERENCES Company(id)
+);
+
+CREATE TABLE ClientDetails (
+    id int NOT NULL PRIMARY KEY,
+	client int NOT NULL,
+	address varchar(30),
+    zip varchar(30),
+    city varchar(30),
+    country varchar(30),
+    email varchar(30),
+    phone varchar(30),
+    vat varchar(30),
+    commerce varchar(30),
+    FOREIGN KEY (client) REFERENCES Client(id)
 );
 
 CREATE TABLE ClientAccount (
@@ -31,35 +80,6 @@ CREATE TABLE Vendor (
     FOREIGN KEY (company) REFERENCES Company(id)
 );
 
-CREATE TABLE Company (
-    id int NOT NULL PRIMARY KEY,
-	name varchar(30) NOT NULL,
-	default_currency int NOT NULL,
-    FOREIGN KEY (default_currency) REFERENCES Currency(id)
-);
-
-CREATE TABLE Employee (
-    id int NOT NULL PRIMARY KEY,
-	company int NOT NULL,
-	user int NOT NULL,
-    FOREIGN KEY (company) REFERENCES Company(id),
-	FOREIGN KEY (user) REFERENCES Users(id)
-);
-
-CREATE TABLE CompanyDetails (
-    id int NOT NULL PRIMARY KEY,
-	company int NOT NULL,
-	address varchar(30),
-    zip varchar(30),
-    city varchar(30),
-    country varchar(30),
-    email varchar(30),
-    phone varchar(30),
-    vat_id varchar(30),
-    commerce_id varchar(30),
-    FOREIGN KEY (company) REFERENCES Company(id)
-);
-
 CREATE TABLE VendorDetails (
     id int NOT NULL PRIMARY KEY,
 	vendor int NOT NULL,
@@ -74,38 +94,13 @@ CREATE TABLE VendorDetails (
     FOREIGN KEY (vendor) REFERENCES Vendor(id)
 );
 
-CREATE TABLE ClientDetails (
+CREATE TABLE Employee (
     id int NOT NULL PRIMARY KEY,
-	client int NOT NULL,
-	address varchar(30),
-    zip varchar(30),
-    city varchar(30),
-    country varchar(30),
-    email varchar(30),
-    phone varchar(30),
-    vat varchar(30),
-    commerce varchar(30),
-    FOREIGN KEY (client) REFERENCES Client(id)
+	company int NOT NULL,
+	user int NOT NULL,
+    FOREIGN KEY (company) REFERENCES Company(id),
+	FOREIGN KEY (user) REFERENCES Users(id)
 );
-
-CREATE TABLE Currency (
-    id int NOT NULL PRIMARY KEY,
-    name varchar(20),
-    code varchar(5)
-);
-
-CREATE TABLE Bill (
-    id int NOT NULL PRIMARY KEY,
-	vendor int NOT NULL,
-	currency int NOT NULL,
-    referance int,
-    uuid varchar(40) NOT NULL,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (vendor) REFERENCES Vendor(id),
-	FOREIGN KEY (currency) REFERENCES Currency(id),
-    FOREIGN KEY (referance) REFERENCES Users(id)
-);
-
 
 CREATE TABLE BankAccount (
     id int NOT NULL PRIMARY KEY,
@@ -117,14 +112,6 @@ CREATE TABLE BankAccount (
     uuid varchar(40) NOT NULL,
     FOREIGN KEY (company) REFERENCES Company(id),
 	FOREIGN KEY (currency) REFERENCES Currency(id)
-);
-
-CREATE TABLE Client (
-    id int NOT NULL PRIMARY KEY,
-	company int NOT NULL,
-	name varchar(40) NOT NULL,
-    uuid varchar(40) NOT NULL,
-    FOREIGN KEY (company) REFERENCES Company(id)
 );
 
 CREATE TABLE Invoice (
@@ -166,6 +153,18 @@ CREATE TABLE Item (
     default_price FLOAT DEFAULT 0,
     uuid varchar(40) NOT NULL,
     FOREIGN KEY (company) REFERENCES Company(id)
+);
+
+CREATE TABLE Bill (
+    id int NOT NULL PRIMARY KEY,
+	vendor int NOT NULL,
+	currency int NOT NULL,
+    referance int,
+    uuid varchar(40) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (vendor) REFERENCES Vendor(id),
+	FOREIGN KEY (currency) REFERENCES Currency(id),
+    FOREIGN KEY (referance) REFERENCES Users(id)
 );
 
 CREATE TABLE BillItem (
